ed23c7cbc899960d50695918c2e7c9fa1f3245d0
ozymandium <robertgcofield@gmail.com>
2012-06-15 12:21:04 -0500

    script now correctly creates an alog with 4 varying sources

--- fhwa2_MOOS_to_ROS/scripts/make_mult_inputs.py
+++ fhwa2_MOOS_to_ROS/scripts/make_mult_inputs.py
@@ -14,56 +14,49 @@ from numpy.random import normal
 
 def main(msg):
 	msg = msg.split()
-	msg1 = msg
-	msg2 = msg
-	msg3 = msg
+	msgs = [msg[:], msg[:], msg[:], msg[:]]
 
-	msg1[2] = 'gPennSt'
-	msg2[2] = 'gSRI'
-	msg3[2] = 'gDSRC'
+	msgs[1][2] = 'gPennSt'
+	msgs[2][2] = 'gSRI'
+	msgs[3][2] = 'gDSRC'
 
 	if ((msg[1] == 'zLat') or (msg[1] == 'zLong')):
 		wiggle = 0.00005 # stdDev of gaussian noise added, in lat/lon decimal degrees ~.5m
-		for value in [msg1[3], msg2[3], msg3[3]]:
-			value = float(value)
-			value += normal(value, wiggle, 1)
-			value = str(value)
 	elif ((msg[1] == 'zLatStdDev') or (msg[1] == 'zLongStdDev')):
 		wiggle = .01 # stdDev of gaussian noise added, in m
-		for value in [msg1[3], msg2[3], msg3[3]]:
-			value = float(value)
-			value += normal(value, wiggle, 1)
-			value = str(value)
 	elif msg[1] == 'zCourse':
 		wiggle = 10 # stdDev of gaussian noise added, in deg
-		for value in [msg1[3], msg2[3], msg3[3]]:
-			value = float(value)
-			value += normal(value, wiggle, 1)
-			value = str(value)
-
-	for message in [msg, msg1, msg2, msg3]:
+		
+	spreadOut(msgs, wiggle)
+	for message in msgs:
 		line = alog_manip.reconstructLine(message)
 		tgt.write(line + '\n')
 
+
+def spreadOut(msgs, wiggle):
+	noises = normal(msgs[0][3], wiggle, len(msgs))
+	for ind in range(len(msgs)):
+		value = float(msgs[ind][3])
+		value += noises[ind]
+		msgs[ind][3] = str(value)
 
 ############################################################################
 
 if __name__ == '__main__':
 	
-	sys.path.append('/home/gavlab/alog_manip/scripts') # alog_manip module
-	sys.path.append('/home/gavlab/alog_manip/alogs') # fhwa2_Novatel.alog (file with only)
-	sys.path.append('/home/gavlab/rgc0003rosws/fhwa2_viz/fhwa2_MOOS_to_ROS/scripts')
-	sys.path.append('/home/gavlab/rgc0003rosws/fhwa2_viz/fhwa2_MOOS_to_ROS/alogs')
+	sys.path.append('/home/ozymandium/alog_manip/scripts') # alog_manip module
+	sys.path.append('/home/ozymandium/alog_manip/alogs') # fhwa2_Novatel.alog (file with only)
+	sys.path.append('/home/ozymandium/fhwa2_ws/fhwa2_viz/fhwa2_MOOS_to_ROS/scripts') # where this is
+	sys.path.append('/home/ozymandium/alog_Files') # location of alog files
 
-	# os.chdir('/home/gavlab/rgc0003rosws/fhwa2_viz/fhwa2_MOOS_to_ROS')
+	# os.chdir('/home/ozymandium/fhwa2_ws/fhwa2_viz/fhwa2_MOOS_to_ROS')
 	import alog_manip
 
 	curdir = os.getcwd()
 	os.chdir('/')
 	
-	NovOnlyAlog = '/home/gavlab/rgc0003rosws/fhwa2_viz/fhwa2_MOOS_to_ROS/alogs/Long_onlyNovatel.alog'
-	ResultAlog = '/home/gavlab/rgc0003rosws/fhwa2_viz/fhwa2_MOOS_to_ROS/alogs/Long_novatel_plus3.alog'
-
+	NovOnlyAlog = '/home/ozymandium/alog_Files/Long_onlyNovatel.alog'
+	ResultAlog = '/home/ozymandium/alog_Files/Long_novatel_plus3.alog'
 	src = open(NovOnlyAlog, 'rU')
 	tgt = open(ResultAlog, 'w')
 	tgt.write('%% This file created to simulate 3 measurment sources varying from the Novatel\n')
@@ -78,4 +71,5 @@ if __name__ == '__main__':
 		else:
 			main(msg)
 
+	print('Writing complete open target *.alog file to confirm')
 	os.chdir(curdir)
