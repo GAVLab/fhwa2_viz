c2a57562fea61fe2f3bb309a8ceeb23f2ef712c3
Robert Cofield <rgc0003@auburn.edu>
2012-07-05 14:49:36 -0500

    removed meshes, worked on increasing msg freq

--- fhwa2_MOOS_to_ROS/scripts/increase_hertz.py
+++ fhwa2_MOOS_to_ROS/scripts/increase_hertz.py
@@ -3,14 +3,18 @@
 This script used to interpolate msgs from generated dummy inputs, creating msgs at ~100Hz
 """
 
-import os, sys,
-sys.path.append('/home/gavlab/alog_manip')
+import os, sys
+sys.path.append('/home/gavlab/alog_manip/scripts')
 import alog_manip as manip
 # from numpy import interp
 from scipy import interpolate
+from pprint import pprint
 
 
+loHzAlog = '/home/gavlab/alog_Files/Long_novatel_plus3.alog'
+loHzDict = manip.alogrd_dict(loHzAlog)
+desHz = 100
+hiHzDict = manip.increaseFreq(loHzDict, desHz)
 
-
-if __name__ == '__main__':
-	pass
+print('\n\n\n\n\nhiHzDict: ')
+# pprint(hiHzDict)
