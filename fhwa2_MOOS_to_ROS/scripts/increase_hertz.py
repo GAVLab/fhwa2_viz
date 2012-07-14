e70849e38ef00b512d982e9f116d9b8529747fe7
Robert Cofield <rgc0003@auburn.edu>
2012-07-13 20:19:41 -0500

    gleck

--- fhwa2_MOOS_to_ROS/scripts/increase_hertz.py
+++ fhwa2_MOOS_to_ROS/scripts/increase_hertz.py
@@ -1,20 +1,17 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 """
 This script used to interpolate msgs from generated dummy inputs, creating msgs at ~100Hz
 """
 
-import os, sys
-sys.path.append('/home/gavlab/alog_manip/scripts')
-import alog_manip as manip
-# from numpy import interp
-from scipy import interpolate
+import sys
+sys.path.append('/home/gavlab/')
+from alog_manip.MOOSalog import MOOSalog
 from pprint import pprint
 
-
-loHzAlog = '/home/gavlab/alog_Files/Long_novatel_plus3.alog'
-loHzDict = manip.alogrd_dict(loHzAlog)
+lo_file = '/home/gavlab/alog_Files/Long_novatel_plus3.alog'
+hi_file = '/home/gavlab/alog_Files/Long_novatel_plus3_100Hz.alog'
 desHz = 100
-hiHzDict = manip.increaseFreq(loHzDict, desHz)
+alog = MOOSalog(lo_file, hi_file)
+alog.increaseFreq(desHz)
 
-print('\n\n\n\n\nhiHzDict: ')
-# pprint(hiHzDict)
+# print('\n\n\nhiHzDict: ')
